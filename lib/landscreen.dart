41b797ce3c75764c99692a8426924aeec2749772
bebo1995 <alberto.arrigoni95@gmail.com>
2025-08-28 23:53:57 +0200

    Updated landscreen

--- lib/landscreen.dart
+++ lib/landscreen.dart
@@ -11,16 +11,27 @@ class _LandScreenState extends State<LandScreen>{
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
-    double buttonsH = screenSize.height * 0.15;
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Placeholder(),
-        SizedBox(
-          height: buttonsH,
-          child:  Placeholder(),
-        )
-      ],
+    double buttonsH = screenSize.height * 0.1;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Universo"),
+        actions: [
+          IconButton(
+            onPressed: ()=>{}, 
+            icon: Icon(Icons.settings))
+        ],
+      ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Placeholder(),
+          SizedBox(
+            height: buttonsH,
+            child:  Placeholder(),
+          )
+        ],
+      ),
     );
   }
 }
