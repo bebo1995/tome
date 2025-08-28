4d27776cb225de9763f64ec6c95a93f54b550a98
bebo1995 <alberto.arrigoni95@gmail.com>
2025-08-28 23:55:20 +0200

    Added homepage

---
+++ lib/homepage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mastertome/landscreen.dart';
+
+class Homepage extends StatelessWidget{
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LandScreen();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mastertome/homepage.dart';
 import 'package:mastertome/landscreen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LandScreen()
+      home: const Homepage()
     );
   }
 }
