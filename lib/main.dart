1d94d9cf8acf860bec21f81aabc937d7b0b3a4b3
bebo1995 <alberto.arrigoni95@gmail.com>
2025-08-29 21:43:49 +0200

    Added navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:mastertome/homepage.dart';
+import 'package:mastertome/tomepage.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+enum Routes{
+  homepage,
+  tomepage
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Object? getArgs(BuildContext context){
+    return ModalRoute.of(context)!.settings.arguments;
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final String appTitle = 'Mastertome';
     return MaterialApp(
-      title: 'Mastertome',
+      title: appTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Homepage()
+      initialRoute: Routes.homepage.name,
+      routes: {
+        Routes.homepage.name: (context) => Homepage(title: (getArgs(context) as HomePageArgs).title,),
+        Routes.tomepage.name: (context) => const TomePage(),
+      },
+      home: Homepage(title: appTitle,)
     );
   }
 }
